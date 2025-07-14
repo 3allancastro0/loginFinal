0cdb2a067eaf107c52febd1de53cbcb8fe121ddb
AllanCastro <allancastrou2025@gmail.com>
2025-07-13 23:02:11 -0600

    main.dart con rutas usando go_router

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
+// Importá las pantallas (las crearás en siguientes commits)
+import 'modulos/login_screen.dart';
+import 'modulos/register_screen.dart';
+import 'modulos/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const LoginScreen()),
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => const RegisterScreen(),
+    ),
+    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+  ],
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+      title: 'LoginF App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
